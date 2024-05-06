c659951754938e516848d1d15713048010f86efe
Gustavo de Medeiros <gustavormedeiros2008@gmail.com>
2024-05-06 15:58:07 -0300

    Camera seguindo personagem
    Além da câmera seguir o player, ela também olha para onde o player olha

---
+++ P.I/Assets/Scripts/CameraTarget.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTarget : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    [SerializeField] Transform player;
+    [SerializeField] float threshold;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPos = (player.position - mousePos) / 2f;
+
+        targetPos.x = Mathf.Clamp(targetPos.x, -threshold + player.position.x, threshold + player.position.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, -threshold + player.position.y, threshold + player.position.y);
+
+        this.transform.position = targetPos;
+    }
+}
