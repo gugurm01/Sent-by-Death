163b97678e2deb440e339e2f11dbf689bb9750d2
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-05-09 11:31:39 -0300

    Bugs da câmera corrigidos
    Também já começamos a implementação da primeira arma

--- P.I/Assets/Scripts/AtiraeMIra.cs
+++ P.I/Assets/Scripts/AtiraeMIra.cs
@@ -5,18 +5,37 @@ using UnityEngine.InputSystem;
 
 public class AtiraeMIra : MonoBehaviour
 {
+    public SpriteRenderer playerSR, gunSR;
+
     [SerializeField] private GameObject gun;
+
+    Transform weaponParent;
 
     private Vector2 worldPosition;
     private Vector2 direction;
+
     private void Update()
     {
         HandleGunRotation();
+
     }
+
     private void HandleGunRotation()
     {
         worldPosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         direction = (worldPosition - (Vector2)gun.transform.position).normalized;
         gun.transform.right = direction;
+
+        Vector2 scale = transform.localScale;
+        if(direction.x < 0)
+        {
+            scale.y = -1;
+        }
+        else if(direction.x > 0)
+        {
+            scale.y = 1;
+        }
+
+        transform.localScale = scale;
     }
 }
