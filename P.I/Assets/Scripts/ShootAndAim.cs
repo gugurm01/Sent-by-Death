1c000654757bd6180ab9f3ebc82b790752b6196b
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-05-10 18:56:02 -0300

    Revolver (bugado)

--- P.I/Assets/Scripts/ShootAndAim.cs
+++ P.I/Assets/Scripts/ShootAndAim.cs
@@ -7,7 +7,8 @@ public class ShootAndAim : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     public int bulletsGun = 5;
-    public GameObject balas1, balas2, balas3, balas4, balas5;
+    private bool block;
+    public float delay;
 
     public void Start()
     {
@@ -15,88 +16,40 @@ public class ShootAndAim : MonoBehaviour
     }
     void Update()
     {
-
-        if (Input.GetButtonDown("Fire1") && bulletsGun >= 1)
-        {
-            FindObjectOfType<AudioManager>().Play("retrogunshot");
-        }
+        if(block == true)
+            return;
         if (Input.GetButtonDown("Fire1"))
         {
+            if(block == true)
+                return;
             Shoot();
             StartCoroutine(reload());
             bulletsGun--;
         }
-        if (bulletsGun == 0)
-        {
-            bulletPrefab.SetActive(false);
-        }
-        if (bulletsGun < 0)
-        {
-            bulletsGun = 0;
-        }
-        print(bulletsGun);
-
-        if (bulletsGun == 0)
-        {
-            balas1.SetActive(false);
-            balas2.SetActive(false);
-            balas3.SetActive(false);
-            balas4.SetActive(false);
-            balas5.SetActive(false);
-        }
-        if (bulletsGun == 1)
-        {
-            balas1.SetActive(true);
-            balas2.SetActive(false);
-            balas3.SetActive(false);
-            balas4.SetActive(false);
-            balas5.SetActive(false);
-        }
-        if (bulletsGun == 2)
-        {
-            balas1.SetActive(true);
-            balas2.SetActive(true);
-            balas3.SetActive(false);
-            balas4.SetActive(false);
-            balas5.SetActive(false);
-        }
-        if (bulletsGun == 3)
-        {
-            balas1.SetActive(true);
-            balas2.SetActive(true);
-            balas3.SetActive(true);
-            balas4.SetActive(false);
-            balas5.SetActive(false);
-        }
-        if (bulletsGun == 4)
-        {
-            balas1.SetActive(true);
-            balas2.SetActive(true);
-            balas3.SetActive(true);
-            balas4.SetActive(true);
-            balas5.SetActive(false);
-        }
-        if (bulletsGun == 5)
-        {
-            balas5.SetActive(true);
-            balas4.SetActive(true);
-            balas3.SetActive(true);
-            balas2.SetActive(true);
-            balas1.SetActive(true);
-        }
+        
     }
     void Shoot()
     {
+        block = true;
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        StartCoroutine(Dalay());
+    }
+
+    IEnumerator Dalay()
+    {
+        yield return new WaitForSeconds(delay);
+        block = false;
     }
     IEnumerator reload()
     {
         if (bulletsGun == 1)
         {
+            block = true;
             yield return new WaitForSeconds(2);
+            block = false;
             bulletsGun = bulletsGun + 5;
             bulletPrefab.SetActive(true);
-            balas1.SetActive(false); balas2.SetActive(false); balas3.SetActive(false); balas4.SetActive(false); balas5.SetActive(false);
+            
         }
     }
 }
