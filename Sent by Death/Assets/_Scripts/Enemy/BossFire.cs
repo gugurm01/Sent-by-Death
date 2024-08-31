66327b9b11eb1399ff2a7d604ddbbbe36b82e689
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-08-31 16:33:27 -0300

    Boss inicio

---
+++ Sent by Death/Assets/_Scripts/Enemy/BossFire.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossFire : MonoBehaviour
+{
+    enum SpawnerType { Straight, Spin }
+
+    [Header("Bullet")]
+    public GameObject bulletPrefab;
+
+    [Header("Spawner")]
+    [SerializeField] SpawnerType type;
+    public float spin;
+    public float fireRate;
+    float timer;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+        if(type == SpawnerType.Spin) { transform.eulerAngles = new Vector3(0f, 0f, transform.eulerAngles.z + spin); }
+        if(timer > fireRate)
+        {
+            Fire();
+            timer = 0;
+        }
+    }
+
+    void Fire()
+    {
+        Instantiate(bulletPrefab, transform.position, transform.rotation);
+    }
+}
