3898c028fc2dc505002ad676edac90e294047aef
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-09-11 08:33:58 -0300

    frafas🧦🩱

---
+++ Sent by Death/Assets/_Scripts/GameManager/Persistence.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Persistence : MonoBehaviour
+{
+    public static Persistence Instance;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
