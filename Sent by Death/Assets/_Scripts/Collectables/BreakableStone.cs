c9bb788811533d4f6488cdb134d816f3a41952f0
Gustavo de Medeiros <143119460+gugurm01@users.noreply.github.com>
2024-10-28 14:39:52 -0300

    bola 8 sinuca

---
+++ Sent by Death/Assets/_Scripts/Collectables/BreakableStone.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableStone : MonoBehaviour
+{
+    public Color blinkColor = Color.red; 
+    public Color originalColor = Color.white; 
+    public float blinkDuration = 0.5f; 
+    public int maxHealth = 3; 
+    private int currentHealth;
+
+    private SpriteRenderer spriteRenderer;
+    public GameObject explosionEffect; 
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = originalColor; 
+        currentHealth = maxHealth;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            TakeDamage();
+        }
+    }
+
+    void TakeDamage()
+    {
+        currentHealth--;
+
+        if (currentHealth <= 0)
+        {
+            Explode();
+        }
+        else
+        {
+            StartCoroutine(Blink());
+        }
+    }
+
+    private IEnumerator Blink()
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < blinkDuration)
+        {
+            spriteRenderer.color = Color.Lerp(originalColor, blinkColor, elapsedTime / blinkDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        spriteRenderer.color = originalColor;
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+    }
+}
